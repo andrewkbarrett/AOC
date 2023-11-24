a60ea8dabeaba6f93814628248067514e548e5b9
Andy Barrett <andyb@Andys-MacBook-Pro.local>
2023-11-23 21:57:51 -0700

    adding required tag to params

--- 2023/template/main.py
+++ 2023/template/main.py
@@ -9,29 +9,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="main.py",
         description="AOC main file <YEAR> <DayX>")
-    parser.add_argument('-t', '--tokenfile', default = "./token.txt" )
-    parser.add_argument('-i', '--input',default = "example.txt")
-    parser.add_argument('-f', '--filetype', default="example")
-    parser.add_argument('-y', '--year')
-    parser.add_argument('-d', '--day')
+    parser.add_argument('-t', '--tokenfile', default = "./token.txt",required=True)
+    parser.add_argument('-i', '--input',default = "example.txt", required=True)
+    parser.add_argument('-f', '--filetype', default="example", required=True)
+    parser.add_argument('-y', '--year',required=True)
+    parser.add_argument('-d', '--day',required=True)
 
     args = parser.parse_args()
 
-    if (args.tokenfile == ""):
-        rprint("token not present - get token and update file")
-    
-    if (args.input == ""):
-        rprint("input file not entered")
-    
-    if (args.filetype == ""):
-        rprint("file type not entered")
-    
-    else:
-        ## check to see if either the example file or input file is there
-        ## if not call up the site to pull down the correct file. 
-        token = utils.load_token(args.tokenfile)
-        print(token[0])
-        out = utils.check_and_load_file(args.filetype,token[0],args.input,args.year,args.day)
-        print(out)
-        ##all is ready, add code to run_solver and execute
-        run_solver()
+    ## check to see if either the example file or input file is there
+    ## if not call up the site to pull down the correct file. 
+    token = utils.load_token(args.tokenfile)
+    print(token[0])
+    out = utils.check_and_load_file(args.filetype,token[0],args.input,args.year,args.day)
+    print(out)
+    ##all is ready, add code to run_solver and execute
+    run_solver()
