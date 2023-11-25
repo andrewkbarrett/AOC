9fa5e6eda9455c38d3aa7f3812117162d726985f
Andy Barrett <andyb@Andys-MacBook-Pro.local>
2023-11-24 19:34:13 -0700

    removing kruft

--- 2023/template/main.py
+++ 2023/template/main.py
@@ -1,27 +1,5 @@
-import argparse
-from rich import print as rprint
-from lib import utils
+from aocd.models import Puzzle
 
-def run_solver():
-    pass
+puzzle = Puzzle(year=2022, day=11)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog="main.py",
-        description="AOC main file <YEAR> <DayX>")
-    parser.add_argument('-t', '--tokenfile', default = "./token.txt",required=True)
-    parser.add_argument('-i', '--input',default = "example.txt", required=True)
-    parser.add_argument('-f', '--filetype', default="example", required=True)
-    parser.add_argument('-y', '--year',required=True)
-    parser.add_argument('-d', '--day',required=True)
-
-    args = parser.parse_args()
-
-    ## check to see if either the example file or input file is there
-    ## if not call up the site to pull down the correct file. 
-    token = utils.load_token(args.tokenfile)
-    print(token[0])
-    out = utils.check_and_load_file(args.filetype,token[0],args.input,args.year,args.day)
-    print(out)
-    ##all is ready, add code to run_solver and execute
-    run_solver()
+print(puzzle.examples)
